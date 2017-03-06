e3420144d047c124d15444089c8134646f8e771b
Jeffrey Brown <Jeffrey Brown>
2017-03-06 00:56:39 -0500

    final with pseudo

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,12 @@
+/* Jeff Brown 2/27 thru 3/6/2017
+ *
+ *Core of game is three arrays
+ *-- puzzleSolution -- array of letters from data name string
+ *-- puzzleState -- initial blank spaces same length are fliped/revealed as letters are guessed correctly
+ */
+
+
+// data in objects
 var dataKyans = [
   {"name":"Jennifer Lawrence", "hint":'Actress<br>Katness in "The Hunger Games"'},
   {"name":"Ashley Judd", "hint":'Actress<br>"Kiss the Girls"<br>"Double Jeopardy"'},
@@ -21,6 +30,7 @@ var priorGuessesText = document.getElementById("prior-guesses-text");
 var priorSelectors = [];
 var puzzleHintText = document.getElementById("puzzle-hint-text");
 var puzzleDisplayText = document.getElementById("puzzle-state-text");
+var puzzleSolution = [];
 var puzzleState = [];
 var puzzleStatusText = document.getElementById("puzzle-status-text");
 var selectorCounter = 0;
@@ -31,25 +41,38 @@ var userTries = 12;
 var userWins = 0;
 var currSolution;
 
+// event handler
 document.onkeyup = function(event) {
+  //capture key code for filtering
   var userInputKeyCode = event.keyCode;
+  // gameState handled in gameHandler()
   if (gameState === "gameover") {
     gameHandler();
+  // always ignore everything except spacebar and letters a-z
   } else if (userInputKeyCode != 32 && (userInputKeyCode < 65 || userInputKeyCode > 90)) {return}
+  // ignore spacebar during game
   if (gameState === "midgame" && userInputKeyCode === 32) {return}
+  // ignore letters before game starts
   if (gameState === "pregame" && userInputKeyCode >= 65 && userInputKeyCode <= 90) {return}
+  // spacebar starts game
   if (userInputKeyCode === 32) {
     gameState = "pregame";
     gameHandler();
+  // letters a-z sent to gameHandler()
   } else {
+  	// convert to char for gameHandler()
     userInput = String.fromCharCode(userInputKeyCode).toLowerCase();
+    // ignore letters already guessed
     if (priorGuesses.length > 0 && priorGuesses.indexOf(userInput) >= 0) {return};
+    // ignore when tries have been exhausted
     if (userTries <= 0) {return}
+    // call gameHandler with user input (duh)
     gameHandler(userInput);
   }
 }
 
 function stringToArray(solutionString) {
+  // convert the randomly selected name to an array of letters
   var tempBuff0 = [];
   for (i = 0; i < solutionString.length; i++) {
     tempBuff0.push(solutionString.charAt(i).toLowerCase());
@@ -58,6 +81,7 @@ function stringToArray(solutionString) {
 }
 
 function isWinner(solution, state) {
+  // winner by definition is when all gameState members are true 
   var tempBoo = true;
   for (i = 0; i < solution.length && tempBoo; i++) {
     tempBoo = tempBoo && state[i];
@@ -76,15 +100,19 @@ function solutionFormatter(solution) {
 }
 
 function gameHandler(input) {
+  // write to display based on game state
   if (gameState === "gameover") {
     puzzleStatusText.innerHTML = "You've completed all<br> the puzzles<br>Game over.";
     triesRemainingText.textContent = "Wins = " + userWins + ", Losses = " + userLosses;
     puzzleDisplayText.innerHTML = "Nice Job! You're now an<br>Honary Kentuckian!";
     priorGuessesText.textContent = "Refresh the page play again";
   } else if (gameState === "pregame") {
+  	// initialize for new game
     priorGuesses = "";
     userTries = 12;
+    // during coding needed a lifeline in case of infinite loop
     var loopBreaker = 0;
+    // pic a random element number
     randomSelector = Math.floor(Math.random() * dataKyans.length);
     while (priorSelectors.indexOf(randomSelector) >= 0) {
       loopBreaker++;
@@ -94,46 +122,71 @@ function gameHandler(input) {
         return
       }
     }
+    // apply random element to solution object
     currSolution = dataKyans[randomSelector];
+    // append selected (random) element index to array
+    // so no duplication of puzzles
+    // note -- puzzles appear randomly but there's only 14 famous people in Kentucky
     priorSelectors.push(randomSelector)
+    // convert name string to array of letters
     puzzleSolution = stringToArray(currSolution.name);
+    // preserve the space for display elegance
     puzzleSolution = solutionFormatter(puzzleSolution);
+    // initialize blank spaces for display
     puzzleState = initState(puzzleSolution);
+    // put the hint in front of the user
     puzzleHintText.innerHTML = currSolution.hint;
+    // give a sense of game progress, item x of y items
     selectorCounter++;
+    // display information
     gameHeadingText.textContent = "Famous Kentuckian #" + selectorCounter + " (of "+ dataKyans.length + ")";
     puzzleStatusText.textContent = writePuzDisTxt(puzzleSolution, puzzleState);
     puzzleDisplayText.innerHTML = "Start guessing letters now...<br>we'll see if they're in the puzzle.";
     triesRemainingText.textContent = "Tries remaining = " + userTries
+    // change game state from new to in progress
     gameState = "midgame";
   } else if (gameState === "midgame") {
+  	// query the puzzle for guess correctness
     puzzleState = attemptHandler(puzzleSolution, puzzleState, input);
+    // refresh puzzle display in it's current state
     puzzleStatusText.textContent = writePuzDisTxt(puzzleSolution, puzzleState);
+    // check for end of this round
+    // first, did they lose the round?
     if (userTries <= 0) {
       puzzleStatusText.textContent = "Oops, you've run out of tries ... Game over.";
+      // display correct answer
       puzzleDisplayText.textContent = "Correct answer = " + currSolution.name;
+      // increment loss counter
       userLosses++;
       triesRemainingText.textContent = "Wins = " + userWins + ", Losses = " + userLosses;
       priorGuessesText.textContent = "Hit the spacebar to play again";
+      // check for end of all items
       if (priorSelectors.length >= dataKyans.length) {
         gameState = "gameover";
       } else {
+      	// pregame state sets up next round
         gameState = "pregame";
       }
+      // check if they won the round
     } else if (isWinner(puzzleSolution, puzzleState)) {
       puzzleDisplayText.textContent = "Winner! Great job!";
+      // display correct answer
       puzzleStatusText.textContent = currSolution.name;
+      // increment win counter
       userWins++;
       triesRemainingText.textContent = "Wins = " + userWins + ", Losses = " + userLosses;
       priorGuessesText.textContent = "Hit the spacebar to play again";
+      // same as above, check for last round
       if (priorSelectors.length >= dataKyans.length) {
         gameState = "gameover";
       } else {
         gameState = "pregame";
       }
     } else {
+      //mid game display
       puzzleDisplayText.textContent = "Keep guessing ...";
       triesRemainingText.textContent = "Tries remaining = " + userTries
+      // add this unsuccessful guess to history
       priorGuessesText.textContent = priorGuesses;
     }
   }
@@ -170,7 +223,6 @@ function attemptHandler(solution, state, guess) {
   // returns state of puzzle after choice
   // array of booleans equal length of solution
   // true if a letter has been matched, false if not
-  // if passed state is empty, initialize all false length of solution
   var guessSuccess = false;
   for (i = 0; i < solution.length; i++) {
     if (solution[i] === guess) {
